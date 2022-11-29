ed8367257e224a76526a14d8ea758701b6e99543
tracer-ics2021 <tracer@njuics.org>
2022-11-29 21:34:56 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 4482728dd945465388fa2fba70140401   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  21:34:56 up 1 day,  4:23,  1 user,  load average: 0.14, 0.30, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -212,6 +212,7 @@ uint32_t eval(uint32_t begin, uint32_t end){
 		Token op = get_main_token(tokens,&op_pos);
 		printf("the current op's position is %d\n",op_pos);
 		uint32_t val1 = eval(begin, op_pos -1);	
+		printf("It can run here!!\n");
 		uint32_t val2 = eval(op_pos + 1,end);	
 		printf("val1 = %d, val2 = %d\n", val1,val2);
 		switch (op.type){
