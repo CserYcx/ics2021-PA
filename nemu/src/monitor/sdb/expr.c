1e4ba909ddd90bb7c423f7f7b5c666d93d2fdf75
tracer-ics2021 <tracer@njuics.org>
2022-12-01 16:58:33 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: aba295f2caad44d098b90eba56769318   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-52-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:58:33 up  4:52,  1 user,  load average: 0.21, 0.16, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -221,9 +221,9 @@ uint32_t eval(uint32_t begin, uint32_t end){
 	else{
 		Log("Begining find the main token!!\n");
 		op_pos = get_main_token(tokens,begin,end,op_pos);
-		printf("the current op's position is %d\n",op_pos);
+		printf("1  the current op's position is %d\n",op_pos);
 		uint32_t val1 = eval(begin, op_pos-1);	
-		printf("the current op's position is %d\n",op_pos);
+		printf("2  the current op's position is %d\n",op_pos);
 		uint32_t val2 = eval(op_pos + 1,end);	
 		printf("val1 = %d, val2 = %d\n", val1,val2);
 		printf("type = %c\n",tokens[op_pos].type);
