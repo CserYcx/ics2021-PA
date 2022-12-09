eb3665590c0aaffd1a26f721adf16ba124ca5dac
tracer-ics2021 <tracer@njuics.org>
2022-12-09 16:52:15 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 8829d635982f4050bb92ba3289f2ff51   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:52:16 up  8:23,  1 user,  load average: 0.19, 0.24, 0.40

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,14 +16,30 @@ static char *code_format =
 "  return 0; "
 "}";
 
+uint32_t choose(uint32_t num){
+	int rand_num = rand() % num;
+	return rand_num;
+}
+
+static void gen_num(){
+	int rand_num = rand();
+		
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  //buf[0] = '\0';
+	switch(choose(3)){
+		case 0: gen_num();break;
+		default:break;
+		}
 }
 
 int main(int argc, char *argv[]) {
+	//time(0) is to return the system's time
   int seed = time(0);
-  srand(seed);
+  srand(seed);// create the random number
   int loop = 1;
+	//loop is how many number you want to create
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
@@ -33,11 +49,13 @@ int main(int argc, char *argv[]) {
 
     sprintf(code_buf, code_format, buf);
 
+		//build a new c txt with the code_format 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
 
+		//execute shell command 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
