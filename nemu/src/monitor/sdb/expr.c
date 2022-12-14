207d70eb2d04d53d2218b26ecab81cb38d64f10b
tracer-ics2021 <tracer@njuics.org>
2022-12-14 14:22:58 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 8829d635982f4050bb92ba3289f2ff51   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  14:22:58 up 17:54,  1 user,  load average: 0.73, 0.43, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -203,7 +203,7 @@ uint32_t get_main_token(Token *token,uint32_t begin,uint32_t end, uint32_t pos){
 
 		// the main token priority is the lowest 
 		if(flag == 1){	
-			if(token[cnt].type <=47||token[cnt].type == TK_EQ || token[cnt].type == TK_NOTEQ || token[cnt].type == TK_AND){
+			if((token[cnt].type > 41 &&token[cnt].type <=47)||token[cnt].type == TK_EQ || token[cnt].type == TK_NOTEQ || token[cnt].type == TK_AND){
 				if(priority > temp_priority){
 					priority = temp_priority;
 					pos = cnt;
