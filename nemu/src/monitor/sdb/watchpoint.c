5d8f98042c23443e4d441f18ba2fab05853f4e99
tracer-ics2021 <tracer@njuics.org>
2022-12-15 16:17:19 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 8829d635982f4050bb92ba3289f2ff51   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-53-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:17:19 up 22:05,  1 user,  load average: 0.08, 0.21, 0.41

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -39,7 +39,6 @@ void init_wp_pool() {
 WP* new_wp(){
 
 	WP* wp = (WP*)malloc(sizeof(WP));
-	assert(wp != NULL);
 	if (free_ != NULL){
 		wp = free_;
 		free_ = free_->next;
@@ -52,8 +51,8 @@ WP* new_wp(){
 		assert(temp != NULL);
 		while(temp != NULL && temp->next != NULL){
 			temp = temp->next;
-			temp->next = wp;
 		}
+		temp->next = wp;
 	}
 	if (head == NULL){
 		head = wp;
