9550f30e0f42f194515ce523bc34d22fa62f7515
tracer-ics2021 <tracer@njuics.org>
2023-01-10 17:34:38 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: ba3d11b82b584a27ba4704c608a726bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:34:38 up  2:09,  1 user,  load average: 1.45, 1.53, 1.37

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -6,8 +6,8 @@ def_EHelper(lui) {
 // addi
 def_EHelper(addi){
   printf("addi : imm is 0x%x\n",id_src1->imm);
-  printf("addi : dsrc1 is 0x%x\n",*dsrc1);
   rtl_addi(s,ddest,dsrc1,id_src1->imm);
+  printf("addi : dsrc1 is 0x%x\n",*dsrc1);
 }
 
 // auipc (immediate + pc -> dest)
