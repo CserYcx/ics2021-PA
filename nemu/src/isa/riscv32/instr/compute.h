edbc9b530a3b3e8b7ad6a76d7262c82f31927974
tracer-ics2021 <tracer@njuics.org>
2023-01-10 16:21:45 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: ba3d11b82b584a27ba4704c608a726bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:21:45 up 56 min,  1 user,  load average: 0.52, 0.63, 0.69

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,16 +1,17 @@
 def_EHelper(lui) {
-  printf("lui: imm is 0x%x",id_src1->imm);
+  printf("lui: imm is 0x%x\n",id_src1->imm);
   rtl_li(s, ddest, id_src1->imm);
 }
 
 // addi
 def_EHelper(addi){
-  printf("addi : imm is 0x%x",id_src1->imm);
+  printf("addi : imm is 0x%x\n",id_src1->imm);
   rtl_addi(s,ddest,dsrc1,id_src1->imm);
 }
 
 // auipc (immediate + pc -> dest)
 def_EHelper(auipc){
-  printf("auipc : imm is 0x%x",id_src1->imm);
+  printf("auipc : imm is 0x%x\n",id_src1->imm);
   rtl_li(s,ddest,id_src1->imm+s->pc);
+  printf("auipc : ddest is 0x%x\n",*ddest);
 }
