58de5f64a3f91150f52b84a40e6a3908c16575f3
tracer-ics2021 <tracer@njuics.org>
2023-01-11 15:04:43 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: ba3d11b82b584a27ba4704c608a726bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:04:44 up  3:26,  1 user,  load average: 1.37, 0.68, 0.50

--- nemu/src/isa/riscv32/instr/cond.h
+++ nemu/src/isa/riscv32/instr/cond.h
@@ -1,10 +1,11 @@
 //jal 
 def_EHelper(jal){
     // in the rtl-basic.h
-    sword_t simm = (s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm10_1 << 9) |
-                 (s->isa.instr.j.imm11 << 8) | (s->isa.instr.j.imm19_12);
-    simm = simm>>8;
-    printf("simm = %x\n",simm);
+    // need to relocate the imm and move left 1 bit
+    sword_t simm = (s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm19_12) << 11
+    | (s->isa.instr.j.imm11 << 2) | (s->isa.instr.j.imm10_1 ) ;
+    simm = simm<<1;
+    printf("jal: simm = %x\n",simm);
     printf("next pc = %x\n",simm + s->pc);
     *ddest = s->pc+4;
     rtl_j(s,s->pc += simm);
