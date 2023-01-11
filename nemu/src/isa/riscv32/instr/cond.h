be581571976c8b606ed578cfd6a316fc9ad817a7
tracer-ics2021 <tracer@njuics.org>
2023-01-11 16:56:42 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:56:43 up 27 min,  1 user,  load average: 1.16, 1.09, 0.77

--- nemu/src/isa/riscv32/instr/cond.h
+++ nemu/src/isa/riscv32/instr/cond.h
@@ -4,10 +4,12 @@ def_EHelper(jal){
     // need to relocate the imm and move left 1 bit
     sword_t simm = (s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm19_12) << 11
     | (s->isa.instr.j.imm11 << 10) | (s->isa.instr.j.imm10_1 ) ;
+    /*
     printf("jal: imm20 is %x\n",s->isa.instr.j.simm20<<19);
     printf("jal: imm19 is %x\n",s->isa.instr.j.imm19_12<<11);
     printf("jal: imm11 is %x\n",s->isa.instr.j.imm11<<2);
     printf("jal: imm10 is %x\n",s->isa.instr.j.imm10_1);
+    */
     simm = simm<<1;
     printf("jal: simm = %x\n",simm);
     printf("next pc = %x\n",simm + s->pc);
@@ -16,7 +18,10 @@ def_EHelper(jal){
 }
 
 // jalr
-/*
 def_EHelper(jalr){
-    rtl_j;
-}*/
+    *ddest = s->pc+4;
+    s->pc = (*dsrc1+s->isa.instr.i.simm11_0) & 0xfffffffe;
+    *dsrc1 = s->pc;
+    printf("jalr: next pc = %x\n",s->pc);
+    rtl_jr(s,dsrc1);
+}
