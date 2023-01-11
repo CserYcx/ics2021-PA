defe52d692488473f88f92bac08dcc3b61e089b9
tracer-ics2021 <tracer@njuics.org>
2023-01-11 16:06:58 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: ba3d11b82b584a27ba4704c608a726bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:06:58 up  4:08,  1 user,  load average: 1.41, 1.11, 0.90

--- nemu/src/isa/riscv32/instr/cond.h
+++ nemu/src/isa/riscv32/instr/cond.h
@@ -4,9 +4,9 @@ def_EHelper(jal){
     // need to relocate the imm and move left 1 bit
     sword_t simm = (s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm19_12) << 11
     | (s->isa.instr.j.imm11 << 2) | (s->isa.instr.j.imm10_1 ) ;
-    printf("jal: imm20 is %x\n",s->isa.instr.j.simm20);
-    printf("jal: imm19 is %x\n",s->isa.instr.j.imm19_12);
-    printf("jal: imm11 is %x\n",s->isa.instr.j.imm11);
+    printf("jal: imm20 is %x\n",s->isa.instr.j.simm20<<19);
+    printf("jal: imm19 is %x\n",s->isa.instr.j.imm19_12<<11);
+    printf("jal: imm11 is %x\n",s->isa.instr.j.imm11<<2);
     printf("jal: imm10 is %x\n",s->isa.instr.j.imm10_1);
     simm = simm<<1;
     printf("jal: simm = %x\n",simm);
