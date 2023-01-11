53c775299492bac2ddf95ae8fd018fd5e9a88916
tracer-ics2021 <tracer@njuics.org>
2023-01-11 15:20:56 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: ba3d11b82b584a27ba4704c608a726bf   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:20:56 up  3:43,  1 user,  load average: 1.89, 1.15, 0.70

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -51,9 +51,9 @@ static def_DHelper(U)
 // Maybe has a bug here: why all the flag state are true
 static def_DHelper(J)
 {
-  sword_t simm = (s->isa.instr.j.simm20 << 20) | (s->isa.instr.j.imm19_12) << 11
+  sword_t simm = (s->isa.instr.j.simm20 << 19) | (s->isa.instr.j.imm19_12) << 11
                 |(s->isa.instr.j.imm11 << 2)   | (s->isa.instr.j.imm10_1 ) ;
-  decode_op_i(s, id_src1, simm, false);
+  decode_op_i(s, id_src1, simm<<1, false);
   decode_op_r(s, id_dest, s->isa.instr.j.rd, true);
 }
 
