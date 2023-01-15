e96c74caf43b5adaae1d78c2785e3dd44c59c975
tracer-ics2021 <tracer@njuics.org>
2023-01-15 16:18:31 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:18:31 up 10:21,  1 user,  load average: 1.24, 1.13, 1.09

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -29,8 +29,8 @@ def_EHelper(auipc){
 
 // add (src1 + src2 -> dest)
 def_EHelper(add){
-  printf("add : src1 is 0x%x\n",id_src1->imm);
-  printf("add : src2 is 0x%x\n",id_src2->imm);
+  printf("add : src1 is 0x%x\n",*dsrc1);
+  printf("add : src2 is 0x%x\n",*dsrc2);
   rtl_add(s,ddest,dsrc1,dsrc2);
-  printf("add : dest is 0x%x\n",id_dest->imm);
+  printf("add : dest is 0x%x\n",*ddest);
 }
--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -1,8 +1,14 @@
 def_EHelper(lw) {
+  printf("lw : src1 is 0x%x\n", *dsrc1);
+  printf("lw : src2 is 0x%x\n", id_src2->imm);
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 4);
+  printf("lw : ddest is 0x%x\n", *ddest);
 }
 
 def_EHelper(sw) {
+  printf("sw : src1 is 0x%x\n", *dsrc1);
+  printf("sw : src2 is 0x%x\n", id_src2->imm);
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
+  printf("sw : ddest is 0x%x\n", *ddest);
 }
 
