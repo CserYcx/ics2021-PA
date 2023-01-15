7267be51dde4bf5258619818614ea8e25765bc63
tracer-ics2021 <tracer@njuics.org>
2023-01-15 15:12:15 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:12:16 up  9:15,  1 user,  load average: 1.26, 0.98, 1.18

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -11,6 +11,7 @@
 #elif defined(__ISA_MIPS32__)
 # define nemu_trap(code) asm volatile ("move $v0, %0; .word 0xf0000000" : :"r"(code))
 #elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+// That's a embeded inline setence
 # define nemu_trap(code) asm volatile("mv a0, %0; .word 0x0000006b" : :"r"(code))
 #elif
 # error unsupported ISA __ISA__
--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,16 +4,20 @@
 extern char _heap_start;
 int main(const char *args);
 
+// point to the begin of heap and end of heap
+// use the heap, we can dynamic allocate the memory for our program
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
+// print out a char
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
 
+// end the program work
 void halt(int code) {
   nemu_trap(code);
 
@@ -21,6 +25,7 @@ void halt(int code) {
   while (1);
 }
 
+// do some TRM initial work
 void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
