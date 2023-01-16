4ed2e4bfb1a9eb60af479f0632275f7d4deca01a
tracer-ics2021 <tracer@njuics.org>
2023-01-16 22:54:41 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  22:54:41 up 14:21,  1 user,  load average: 0.71, 0.75, 0.89

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -262,7 +262,7 @@ static int cmd_d(char* args){
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) //{ return; }
+        if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
     }
