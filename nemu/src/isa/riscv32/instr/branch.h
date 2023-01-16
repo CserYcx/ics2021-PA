49b9fdf90d24377fbc7719d09bbf4e1039d58947
tracer-ics2021 <tracer@njuics.org>
2023-01-16 16:34:29 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:34:29 up 12:30,  1 user,  load average: 0.40, 0.78, 0.90

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -1,14 +1,14 @@
 // beq
 def_EHelper(beq){
+    printf("beq : dsrc1 is 0x%x\n",*dsrc1);
+    printf("beq : dsrc2 is 0x%x\n",*dsrc2);
     if(*dsrc1 == *dsrc2){
     sword_t simm = (s->isa.instr.b.simm12 << 11)|(s->isa.instr.b.imm11 << 10)|
                    (s->isa.instr.b.imm10_5 << 6)|(s->isa.instr.b.imm4_1 << 1);
     printf("beq : offset is 0x%x\n",simm);
-        printf("beq : dsrc1 is 0x%x\n",*dsrc1);
-        printf("beq : dsrc2 is 0x%x\n",*dsrc2);
-        printf("beq : next pc is 0x%x\n",s->pc + simm);
+    printf("beq : next pc is 0x%x\n",s->pc + simm);
         rtl_j(s,s->pc += simm);
     }else{
-        printf("beq : src1 not equal to src2");
+        printf("beq : src1 not equal to src2\n");
     }
 }
