a19a3b48638a2901259c6766d1d6b7c06f7189d5
tracer-ics2021 <tracer@njuics.org>
2023-01-16 17:48:03 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:48:03 up 13:44,  1 user,  load average: 1.25, 0.66, 0.81

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -17,7 +17,7 @@ def_EHelper(beq){
 def_EHelper(bne){
     printf("bne : dsrc1 is 0x%x\n",*dsrc1);
     printf("bne : dsrc2 is 0x%x\n",*dsrc2);
-    if(*dsrc1 == *dsrc2){
+    if(*dsrc1 != *dsrc2){
     sword_t simm = (s->isa.instr.b.simm12 << 11)|(s->isa.instr.b.imm11 << 10)|
                    (s->isa.instr.b.imm10_5 << 6)|(s->isa.instr.b.imm4_1 << 1);
     printf("bne : offset is 0x%x\n",simm);
