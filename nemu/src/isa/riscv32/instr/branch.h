37f2ba1a9d289b8ae1515998fc4d74f0c47672d5
tracer-ics2021 <tracer@njuics.org>
2023-01-17 17:05:24 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:05:24 up 17:38,  1 user,  load average: 1.65, 0.81, 0.60

--- nemu/src/isa/riscv32/instr/branch.h
+++ nemu/src/isa/riscv32/instr/branch.h
@@ -28,11 +28,11 @@ def_EHelper(bne){
     }
 }
 
-// bge
+// bge (signed)
 def_EHelper(bge){
     printf("bge : dsrc1 is 0x%x\n",*dsrc1);
     printf("bge : dsrc2 is 0x%x\n",*dsrc2);
-    if(*dsrc1 >= *dsrc2){
+    if((int32_t)*dsrc1 >= (int32_t)*dsrc2){
     sword_t simm = (s->isa.instr.b.simm12 << 12)|(s->isa.instr.b.imm11 << 11)|
                    (s->isa.instr.b.imm10_5 << 5)|(s->isa.instr.b.imm4_1 << 1);
     printf("bge : offset is 0x%x\n",simm);
@@ -43,11 +43,11 @@ def_EHelper(bge){
     }
 }
 
-// blt
+// blt (signed)
 def_EHelper(blt){
     printf("blt : dsrc1 is 0x%x\n",*dsrc1);
     printf("blt : dsrc2 is 0x%x\n",*dsrc2);
-    if(*dsrc1 < *dsrc2){
+    if((int32_t)*dsrc1 < (int32_t)*dsrc2){
     sword_t simm = (s->isa.instr.b.simm12 << 12)|(s->isa.instr.b.imm11 << 11)|
                    (s->isa.instr.b.imm10_5 << 5)|(s->isa.instr.b.imm4_1 << 1);
     printf("blt : offset is 0x%x\n",simm);
