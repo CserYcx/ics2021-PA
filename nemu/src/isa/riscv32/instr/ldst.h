93ad491872dce6837cbd3d7fe46b139e21a7c26d
tracer-ics2021 <tracer@njuics.org>
2023-01-19 09:34:56 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  09:34:57 up 20:39,  1 user,  load average: 1.16, 1.39, 1.31

--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -4,12 +4,20 @@ def_EHelper(lw) {
   printf("lw : ddest is 0x%x\n", *ddest);
 }
 
+// lbu (load 8 bit from the memory but then zero extended before storing in rd)
+def_EHelper(lbu) {
+  printf("lbu : offset is 0x%x\n", id_src2->imm);
+  rtl_lm(s, ddest, dsrc1, id_src2->imm, 1);
+  printf("lbu : ddest is 0x%x\n", *ddest);
+}
+
+// store word
 def_EHelper(sw) {
   printf("sw : offset is 0x%x\n", id_src2->imm);
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
 
-// sh
+// store 16-bit
 def_EHelper(sh) {
   printf("sh : offset is 0x%x\n", id_src2->imm);
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 2);
