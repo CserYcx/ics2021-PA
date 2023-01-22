15d8805c4cc22a2f49285657cc28f7bfd3026d67
tracer-ics2021 <tracer@njuics.org>
2023-01-22 09:53:14 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  09:53:14 up 1 day, 56 min,  1 user,  load average: 0.57, 0.72, 0.98

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -20,8 +20,33 @@ char *strcat(char *dst, const char *src) {
   panic("Not implemented");
 }
 
+/*
+ * return integer to indicate the result of comparsion
+ * 1. 0, if s1 equals to s2
+ * (return the first symbol ascii D-value)
+ * 2. a negative value if s1 is less than s2
+ * 3. a positive value if s1 is greater than s2
+ */
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int s1len = sizeof(s1);
+  int s2len = sizeof(s2);
+  if (s1len < s2len){
+    return ' '-s2[s1len];
+  }
+  else if (s1len > s2len){
+    return s1[s2len];
+  }
+  else{
+    for(int i = 0;i<s1len;++i){
+      if(s1[i] == s2[i]){
+        continue;
+      }
+      else {
+        return s1[i] - s2[i];
+      }
+    }
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
