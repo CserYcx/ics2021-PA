f03dfd66e5be882458281b2699c25b67fdadfde8
tracer-ics2021 <tracer@njuics.org>
2023-01-22 15:31:36 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:31:37 up 1 day,  2:29,  1 user,  load average: 2.51, 1.99, 1.62

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,11 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(src != NULL && dst != NULL);
+  char *ret = dst;
+  // original function will be overflow, here is similar
+  while((*dst++ = *src++) != '\0');
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,7 +21,15 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  assert(dst != NULL && src != NULL);
+  int srclen = sizeof(src);
+  int dstlen = sizeof(dst);
+  int i = 0;
+  for(;i<srclen && src[i] != '\0';++i){
+    dst[dstlen + i] = src[i];
+  }
+  dst[dstlen + i] = '\0';
+  return dst;
 }
 
 /*
@@ -28,6 +40,7 @@ char *strcat(char *dst, const char *src) {
  * 3. a positive value if s1 is greater than s2
  */
 int strcmp(const char *s1, const char *s2) {
+  assert(s1 != NULL && s2 != NULL);
   int s1len = sizeof(s1);
   int s2len = sizeof(s2);
   if (s1len < s2len){
@@ -54,7 +67,14 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if(!n){
+    return 0;
+  }
+  char *xs = (char *)s;
+  for(int i=0;i<n;++i){
+    *(xs+i) = c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -66,7 +86,16 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if(!n){
+    return 0;
+  }
+  char *xs1 = (char *)s1;
+  char *xs2 = (char *)s2;
+  while(n-- && *xs1 == *xs2){
+    xs1 += 1;
+    xs2 += 1;
+  }
+  return (*(unsigned char *)xs1 - *(unsigned char *)xs2);
 }
 
 #endif
