345cbe26b26c29b2c80ba49410678337afbbb43f
tracer-ics2021 <tracer@njuics.org>
2023-01-23 10:06:18 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  10:06:18 up 1 day,  4:28,  1 user,  load average: 0.99, 1.01, 1.14

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -41,19 +41,18 @@ char *strcat(char *dst, const char *src) {
  */
 int strcmp(const char *s1, const char *s2) {
   assert(s1 != NULL && s2 != NULL);
-  return 100;
-  const unsigned char *p1 = (const unsigned char *)s1; 
-  const unsigned char *p2 = (const unsigned char *)s2; 
-  unsigned char c1, c2;
+  const char *p1 = (const char *)s1; 
+  const char *p2 = (const char *)s2; 
+  char c1, c2;
   do{
-    c1 = (unsigned char) *p1++;
-    c2 = (unsigned char) *p2++;
+    c1 = (char) *p1++;
+    c2 = (char) *p2++;
     if(c1 == '\0'){
       return c1 - c2;
     }
   }while(c1 == c2);
 
-  return 0;
+  return c1 - c2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
