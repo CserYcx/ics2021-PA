3790410cb29c40a744aabef83f0ee5741ec349b6
tracer-ics2021 <tracer@njuics.org>
2023-01-24 17:21:48 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 5d3455b835dd4b4696d42d00ef3c99ca   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-57-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:21:49 up 1 day,  6:42,  1 user,  load average: 1.91, 1.75, 1.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,40 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // variadic list
+  va_list ap;
+  char *ret = out;
+  va_start(ap,fmt);
+  for(int i = 0;i<sizeof(fmt);++i){
+    char *s;
+    if(fmt[i] == '%'){
+      switch (fmt[i+1])
+      {
+      case 'd':
+        s = va_arg(ap,char *);
+        strcpy(ret,s);
+        ret += sizeof(s);
+        ++i;
+        break;
+      
+      case 's':
+        s = va_arg(ap,char *);
+        strcpy(ret,s);
+        ret += sizeof(s);
+        ++i;
+        break;
+      
+      default:
+        panic("To be completed");
+        break;
+      }
+    }
+    else{
+      *ret++ = fmt[i];
+    }
+  }
+  va_end(ap);
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
