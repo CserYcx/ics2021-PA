59785667ca63f979410f3d0e0453383f5fd7f55f
tracer-ics2021 <tracer@njuics.org>
2023-02-03 15:35:24 +0800

    >  compile 201220000  Static hostname: cracker-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: a06506d912724a61b4673951213bbb86          Boot ID: 85525c38b79848daa54f3fbd27e9cb10   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-58-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  15:35:24 up 27 min,  1 user,  load average: 1.21, 1.01, 1.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -38,7 +38,9 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  printf("Paddr_read here: 0x%08x\n",addr);
+  #ifdef CONFIG_MTRACE
+    printf("Paddr_read %d-word here: 0x%08x\n",len,addr);
+  #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
@@ -46,6 +48,9 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+    printf("Paddr_write %d-word here: 0x%08x\n",len,addr);
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
     panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
